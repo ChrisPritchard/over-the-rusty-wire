e31711fe06ead68b008ffb0cb8a1b07c4cc211ad
Christopher <chris@grislygrotto.nz>
2024-01-21 21:31:56 +1300

    more interim work, harder than it seems

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,25 @@ fn ssh_session(username: &str, password: &str) -> Result<Session> {
     Ok(session)
 }
 
-fn read_all(channel: &mut Channel) -> Result<String> {
-    let mut result = Vec::new();
-    let mut buf = [0u8; 1024];
+fn read_until(channel: &mut Channel, finished_token: &str) -> Result<String> {
+    let mut result = String::new();
+    while !result.contains(finished_token) {
 
-    loop {
-        let amount_read = channel.read(&mut buf)?;
-        result.extend_from_slice(&buf[0..amount_read]);
-        if amount_read < buf.len() {
-            break;
+        let mut full_buf = Vec::new();
+        let mut buf = [0u8; 1024];
+
+        loop {
+            let amount_read = channel.read(&mut buf)?;
+            full_buf.extend_from_slice(&buf[0..amount_read]);
+            if amount_read < buf.len() {
+                break;
+            }
         }
-    }
 
-    let decoded = std::str::from_utf8(&result)?;
-    Ok(decoded.to_string())
+        let decoded = std::str::from_utf8(&full_buf)?;
+        result += decoded;
+    }
+    Ok(result)
 }
 
 fn behemoth0() -> Result<String> {
@@ -47,14 +52,13 @@ fn behemoth0() -> Result<String> {
     // upon submitting the real password, it will open a shell
 
     let session = ssh_session("behemoth0", "behemoth0")?;
+    //session.set_blocking(false);
+
     let mut channel = session.channel_session()?;
     channel.request_pty("xterm", None, Some((80, 24, 0, 0)))?;
     channel.shell()?;
 
-    let mut result = String::new();
-    while !result.contains("behemoth0@gibson:~$ ") {
-        result = read_all(&mut channel)?;
-    }
+    let _ = read_until(&mut channel, "behemoth0@gibson:~$ ");
 
     let test_pass = "test";
     let test_cmd = format!("echo {test_pass} | ltrace /behemoth/behemoth0 2>&1");
@@ -63,24 +67,25 @@ fn behemoth0() -> Result<String> {
     channel.write(format!("{test_cmd}\n").as_bytes())?;
     channel.flush()?;
 
-    let result = read_all(&mut channel)?;
+    let result = read_until(&mut channel, "behemoth0@gibson:~$ ")?;
+    let result = result.split("\n").skip(1).find(|s| s.contains(test_pass)).unwrap();
     println!("{result}");
-    let result = read_all(&mut channel)?;
+    let real_pass = result.split("\"").nth(3).unwrap(); // strcmp("my_pass", "real_pass")
+    println!("real pass is '{real_pass}'");
+
+    let real_cmd = format!("echo {real_pass} | /behemoth/behemoth0");
+    println!("running '{real_cmd}'");
+
+    channel.write(format!("{real_cmd}\n").as_bytes())?;
+    channel.flush()?;
+    
+    let result = read_until(&mut channel, "$ ")?;
     println!("{result}");
 
-    // let real_pass = result.split("\"").nth(3).unwrap(); // strcmp("my_pass", "real_pass")
-    // println!("real pass is '{real_pass}'");
-
-    // let real_cmd = format!("echo {real_pass} | /behemoth/behemoth0");
-    // println!("running '{real_cmd}'");
-    // write!(&mut channel, "{real_cmd}\n")?;
-    // let result = read_all(&mut channel)?;
-    // println!("{result}");
-
-    // println!("retrieving /etc/behemoth_pass/behemoth1");
-    // write!(&mut channel, "cat /etc/behemoth_pass/behemoth1\n")?;
-    // let result = read_all(&mut channel)?;
-    // println!("retrieved behemoth1 pass '{result}'");
+    println!("retrieving /etc/behemoth_pass/behemoth1");
+    channel.write(format!("cat /etc/behemoth_pass/behemoth1\n").as_bytes())?;
+    let result = read_until(&mut channel, "$ ")?;
+    println!("retrieved behemoth1 pass '{result}'");
 
     channel.send_eof()?;
     channel.wait_close()?;
