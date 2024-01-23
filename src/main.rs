885b59fc9bee9ce9f20105949aa71f786a4c1684
Christopher <chris@grislygrotto.nz>
2024-01-24 09:34:09 +1300

    minor alterations

--- src/main.rs
+++ src/main.rs
@@ -68,25 +68,24 @@ fn behemoth0(password: &str) -> Result<String> {
     let _ = read_until(&mut channel, "behemoth0@gibson:~$ ");
 
     let test_pass = "test";
+
     let test_cmd = format!("echo {test_pass} | ltrace /behemoth/behemoth0 2>&1");
     println!("running '{test_cmd}'");
-
     write_line(&mut channel, &test_cmd)?;
 
     let result = read_until(&mut channel, "behemoth0@gibson:~$ ")?;
     let result = result.split("\n").skip(1).find(|s| s.contains(test_pass)).unwrap();
     println!("{result}");
+
     let real_pass = result.split("\"").nth(3).unwrap(); // strcmp("my_pass", "real_pass")
     println!("real pass is '{real_pass}'");
 
     let real_cmd = "/behemoth/behemoth0";
-    println!("running '{real_cmd}'");
-
+    println!("running '{real_cmd}' to spawn suid shell");
     write_line(&mut channel, &real_cmd)?;
     
     let _ = read_until(&mut channel, "Password: ")?;
     write_line(&mut channel, &real_pass)?;
-
     let _ = read_until(&mut channel, "$ ")?;
 
     println!("retrieving /etc/behemoth_pass/behemoth1");
